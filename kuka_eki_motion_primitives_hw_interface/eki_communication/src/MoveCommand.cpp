49c768e8e118587ef22a5b2683e1b59b01724818
Ardeshir Talaei <ardeshir.talaei@gmail.com>
2026-06-08 12:16:17 +0200

    Add external axis to KRL program

--- kuka_eki_motion_primitives_hw_interface/eki_communication/src/MoveCommand.cpp
+++ kuka_eki_motion_primitives_hw_interface/eki_communication/src/MoveCommand.cpp
@@ -59,14 +59,14 @@ void rbt::MoveCommand::to_xml(XmlWriter &writer) const
                                  {"Velocity", std::to_string(velocity)},
                                  {"Acceleration", std::to_string(acceleration)},
                                  {"Blending", std::to_string(blending)},
-                                 {"WaitForGripper", std::to_string(wait_for_gripper});
+                                 {"WaitForGripper", std::to_string(wait_for_gripper)}});
 
     writer.add_element("Joint", {{"A1", std::to_string(target_joints.a1)},
                                  {"A2", std::to_string(target_joints.a2)},
                                  {"A3", std::to_string(target_joints.a3)},
                                  {"A4", std::to_string(target_joints.a4)},
                                  {"A5", std::to_string(target_joints.a5)},
-                                 {"A6", std::to_string(target_joints.e6)}});
+                                 {"A6", std::to_string(target_joints.a6)}});
 
     writer.add_element("Cartesian", {{"X", std::to_string(target_cartesian.x)},
                                      {"Y", std::to_string(target_cartesian.y)},
